a0af7afde26e287395e4fd88ed245e30898c865a
= <=>
2020-04-06 11:30:56 -0400

    Add backup script

--- gen-passwords.bash
+++ gen-passwords.bash
@@ -9,5 +9,11 @@ echo -n `openssl rand -base64 20` > mysql/mysql_root_password
 echo -n `openssl rand -base64 20` > mysql/wp_db_password
 cp -p mysql/wp_db_password wordpress/wp_db_password
 
+# update the .my.cnf file with the root password
+sed -i -e "s|password = .*|password = \'$(cat mysql/mysql_root_password)\'|" mysql/.my.cnf
+
 # update the apmia mysql bundle.properties with this password
-sed -i -e s/wordpressdb.password=.*/wordpressdb.password="$(cat mysql/wp_db_password)"/ apmia/mysql.bundle.properties
+sed -i -e "s|wordpressdb.password=.*|wordpressdb.password=\'$(cat mysql/wp_db_password)\'|" apmia/mysql.bundle.properties
+
+# update this install directory in the backup script
+sed -i -e "s|EASY_HOME=.*|EASY_HOME=$PWD|" backup/backup_wordpress
